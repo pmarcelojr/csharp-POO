fe29193485bf7321bc4bb8171ad5a13ac1946b72
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-12 15:52:18 -0300

    criando arquivo 01Conceitos

---
+++ 01Conceitos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
