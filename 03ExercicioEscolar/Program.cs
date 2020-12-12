9af0f5cc31eaec080ce647bbaf1280c73279cb1c
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-12 16:12:01 -0300

    criando arquivo 03ExercicioEscolar

---
+++ 03ExercicioEscolar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03ExercicioEscolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
