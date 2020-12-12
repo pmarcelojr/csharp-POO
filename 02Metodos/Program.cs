c8865c910aa7f8e76bfe675c97856107e7d461ea
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-12 16:10:08 -0300

    criando arquivo e classes 02Metodos

---
+++ 02Metodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Apresentar();
+            p1.Apresentar("Marcelo");
+            p1.Apresentar("David", 30);
+        }
+    }
+}
