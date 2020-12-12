c014ee550c3b84cd513be610d73e6a5160e5d242
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-12 17:59:15 -0300

    criando pasta heranca

---
+++ 010Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _010Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
