75f629b9a46688affbce12bf3c5f3ba5cfc70ad9
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 12:27:46 -0300

    criando arquivos estaticos

---
+++ 012Estaticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _012Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
