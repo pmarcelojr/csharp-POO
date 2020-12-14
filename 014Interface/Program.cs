97ee53edaedb618ed399dc4ab2ecff67eb254349
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 12:57:34 -0300

    criando arquivo de interfaces

---
+++ 014Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _014Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
