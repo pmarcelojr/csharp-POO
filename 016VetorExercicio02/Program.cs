bfbbb9284a5cac2acf059ac47cf3eb32785ee0e2
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-15 17:02:38 -0300

    criando arquivo 016VetorExer02

---
+++ 016VetorExercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _016VetorExercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
