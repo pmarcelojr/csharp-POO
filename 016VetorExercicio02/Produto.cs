4d287d2039857d076b495c58452ee20a5a32078f
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-15 17:24:51 -0300

    resolucao do exer02 de vetor

---
+++ 016VetorExercicio02/Produto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace _016VetorExercicio02
+{
+    class Produto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
--- 016VetorExercicio02/Program.cs
+++ 016VetorExercicio02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _016VetorExercicio02
 {
@@ -6,7 +7,28 @@ namespace _016VetorExercicio02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Qual a quantidade de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.Write("Entre com o nome do produto: ");
+                string name = Console.ReadLine();
+                Console.Write("Entre com o preço do produto: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0 ; i < vect.Length; i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"AVERAGE PRICE = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }   
     }
 }
