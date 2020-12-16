62518b2aac34b7840112942c4527037b082995b4
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-16 12:06:16 -0300

    criando exercicio 01 Bonus de natal conceitos

---
+++ ExerciciosGerais/01ConceitosIniciais/bonusNatal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+    Uma empresa atribui aos seus colaboradores um bónus de Natal que é função do número de filhos e do número de anos de serviço, calculado da seguinte forma:
+- 20 $ por filho
+- 10 $ por ano de serviço
+O programa lê o nº de filhos e o nº de anos de serviço do colaborador e calcula o valor do bónus que lhe deve ser atribuído
+*/
+
+namespace bonusNatal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero de filhos");
+            int filhos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero de anos de serviço: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            int bonus = 20 * filhos;
+            bonus = bonus + (anos * 10);
+            Console.WriteLine($"O valor do bonus é {bonus}");
+        }
+    }
+}
