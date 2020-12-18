1f96624146d13617db1099f62160c55323f3c80d
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-18 18:05:36 -0300

    criando projeto 06Enum1

---
+++ ExerciciosGerais/06Enum1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
