dce9670caf0f2ede34785e62df3935981d7e0e61
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-17 21:47:23 -0300

    criando matriz e preenchendo ela com for

--- ExerciciosGerais/04Matrizes/Program.cs
+++ ExerciciosGerais/04Matrizes/Program.cs
@@ -6,7 +6,21 @@ namespace _04Matrizes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Entre com um numero inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n , n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Clear();
+                Console.Write("Entre com os numeros escolhidos na mesma linha: ");
+                string[] values = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
         }
     }
 }
