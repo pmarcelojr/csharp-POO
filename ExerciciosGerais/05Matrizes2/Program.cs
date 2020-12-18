772ec9b0f7e7714b537e84ae33befe8a140c4caa
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-17 23:25:24 -0300

    preenchendo matriz de projeto 05matrizes2

--- ExerciciosGerais/05Matrizes2/Program.cs
+++ ExerciciosGerais/05Matrizes2/Program.cs
@@ -6,7 +6,25 @@ namespace _05Matrizes2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.Write("Entre com o numero de linhas da matriz: ");
+            int L = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o numero de colunas da matriz: ");
+            int C = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            int[,] mat = new int[L,C];
+            // Preenchendo a matriz
+            for(int i = 0; i < L; i++)
+            {
+                Console.Write($"Entre com os numeros da linha {i}: ");
+                string[] values = Console.ReadLine().Split(' ');
+                for(int j = 0; j < C; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
         }
     }
 }
