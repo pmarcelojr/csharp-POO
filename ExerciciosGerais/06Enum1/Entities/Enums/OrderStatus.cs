55368223a8970acda8d0d093c39ce96b397ac294
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-18 18:14:15 -0300

    criando enum OrderStatus

---
+++ ExerciciosGerais/06Enum1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _06Enum1.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
