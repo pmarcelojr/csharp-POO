5241c5e141b43cb91d1635710a88ab7648816f30
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-19 12:07:52 -0300

    creating game number guessing exercicios day 07

---
+++ ExerciciosGerais/02NumberGuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02NumberGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
