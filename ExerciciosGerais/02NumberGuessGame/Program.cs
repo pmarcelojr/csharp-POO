1cbecfa155e8fa6b54a72c8c95f6b59e92cfeeec
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-19 12:34:50 -0300

    creating logic game number guess

--- ExerciciosGerais/02NumberGuessGame/Program.cs
+++ ExerciciosGerais/02NumberGuessGame/Program.cs
@@ -6,7 +6,28 @@ namespace _02NumberGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Random r = new Random();
+            int winNum = r.Next(0, 10);
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Entre com um numero entre 0 e 100: ");
+                string i = Console.ReadLine();
+                int n = int.Parse(i);
+
+                if(n > winNum)
+                    Console.WriteLine("Numero acima!");
+                else if (n < winNum)
+                    Console.WriteLine("Numero abaixo!");
+                else if (n == winNum)
+                {
+                    Console.WriteLine("Você é o Vencedor!!!");
+                    win = true;
+                }
+            } while (win == false);
+            
         }
     }
 }
