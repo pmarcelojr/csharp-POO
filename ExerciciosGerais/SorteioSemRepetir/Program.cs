8d40af260df573b3c52e037413d8c037fbc50f30
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-22 17:49:20 -0300

    creating system logic program

--- ExerciciosGerais/SorteioSemRepetir/Program.cs
+++ ExerciciosGerais/SorteioSemRepetir/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SorteioSemRepetir
 {
@@ -6,7 +7,35 @@ namespace SorteioSemRepetir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Sorteando dez números que estejam entre o intervalo de 1 a 10.
+            Console.WriteLine(string.Join(", ", SorteiaNumerosSemRepetir(10, 1, 10)));
+        }
+
+        private static int[] SorteiaNumerosSemRepetir(int qtd, int min, int max)
+        {
+            // Validações dos argumentos
+            if (qtd < 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero");
+            else if (qtd > max + 1 - min)
+                throw new ArgumentException("Quantidade deve ser maior do que a diferença entre máximo e mínimo");
+            else if (max <= min)
+                throw new ArgumentException("Máximo deve ser maior do que o mínimo");
+
+            List<int> numerosSorteados = new List<int>();
+            Random rnd = new Random();
+
+            while (numerosSorteados.Count < qtd)
+            {
+                int numerosSorteado = rnd.Next(min, max + 1);
+
+                // Numero já foi sorteado? Entao sorteamos novamente ate o numero não ter sido sorteado ainda.
+                while (numerosSorteados.Contains(numerosSorteado))
+                    numerosSorteado = rnd.Next(min, max + 1);
+                
+                numerosSorteados.Add(numerosSorteado);
+            }
+
+            return numerosSorteados.ToArray();
         }
     }
 }
