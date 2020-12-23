b44304862af64595261a2219dab8f2b6ddf9c997
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-22 23:05:19 -0300

    creating book class

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,10 +7,21 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0 )
-                Console.WriteLine($"Hello {args[0]}");
-            else
-                Console.WriteLine("Hello");
+            var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(101);
+
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+            
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
