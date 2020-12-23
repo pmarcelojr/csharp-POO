010dddcd08ef9e3c0aaace11bcd5ae03613e3989
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-23 20:29:07 -0300

    working with value types and value type parameters

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,23 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(x);
+
+            Assert.Equal(3, x);
+        }
+        private void SetInt(int x)
+        {
+            x = 42;
+        }
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
         public void CSharpCanPassByRef()
         {
             var book1 = GetBook("Book 1");
