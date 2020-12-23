c8fc22ca24f2810a77be5b855709e6cbb1248c34
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-22 23:23:56 -0300

    creating lowgrade and highgrade

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,20 +7,26 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(101);
+            book.AddGrade(77.5);
 
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
             
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach (var number in grades)
             {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
                 result += number;
             }
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The High grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}");
         }
     }
