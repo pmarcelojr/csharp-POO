cb67e6a2e9cb49563797839d5b7adb47ce88224b
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-23 00:05:30 -0300

    add reference to project GradeBook.csproj

---
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrage
+            var book = new Book("");
+            // act
+            
+            // assert
+            
+        }
+    }
+}
