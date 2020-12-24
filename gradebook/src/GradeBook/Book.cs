309539733ef24f47352ad66a0dccb54bfb0e3328
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-24 11:39:45 -0300

    condition in method AddGrade

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -12,7 +12,12 @@ namespace GradeBook
         }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >=0)
+                grades.Add(grade);
+            else
+            {
+                Console.WriteLin("Invalid Value");
+            }
         }
 
         public Statistics GetStatistics()
