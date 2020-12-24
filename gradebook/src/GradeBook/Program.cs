95cec808dceab39896d5e60d33ef7ddfb5fb65ca
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-24 16:02:18 -0300

    Refactory code program in creating method EnterGrades

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,40 +10,44 @@ namespace GradeBook
             var book = new Book("Scott's Grade Book");
             book.GradeAdded += OnGradeAdded;
 
-            while(true)
-            {
-                Console.Write("Enter a grade or 'q' to quit: ");
-                var input = Console.ReadLine();
+            EnterGrades(book);
 
-                if(input == "q")
-                    break;
-                
-                try
-                {
-                    var grade = double.Parse(input);
-                    book.AddGrade(grade);
-                }
-                catch(ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                catch(FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                   Console.WriteLine("*********************************");
-                }
-            }
+            var stats = book.GetStatistics();
 
-            var stats = book.GetStatistics(); 
-                        
             Console.WriteLine(Book.CATEGORY);
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The High grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+        private static void EnterGrades(Book book)
+        {
+            while (true)
+            {
+                Console.Write("Enter a grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                    break;
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("*********************************");
+                }
+            }
         }
 
         static void OnGradeAdded(Object sender, EventArgs e)
