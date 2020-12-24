bd6f2ba07a1a898832a8bab68b66f2bd691e2db3
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-24 11:57:39 -0300

    creating structure addition result letter

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -10,13 +10,30 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
             if(grade <= 100 && grade >=0)
                 grades.Add(grade);
             else
             {
-                Console.WriteLin("Invalid Value");
+                Console.WriteLine("Invalid Value");
             }
         }
 
@@ -34,7 +51,28 @@ namespace GradeBook
                 result.Average += grade;
             }
             result.Average /= grades.Count;
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
 
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
             return result;
         }
 
