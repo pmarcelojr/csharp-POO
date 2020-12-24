af6972bdb7977e7d8342f8b8421458ec593b686b
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-24 15:49:46 -0300

    deriving from a base class NamedObject

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 namespace GradeBook
 {
     public delegate void GradeAddedDelegate(Object sender, EventArgs args);
-    public class Book
+
+    public class NamedObject
+    {
+        public string Name { get; set; }
+    }
+
+    public class Book : NamedObject
     {
         public Book(string name)
         {
@@ -85,7 +91,6 @@ namespace GradeBook
         }
 
         private List<double> grades;
-        public string Name { get; set; }
 
         public const string CATEGORY = "Science";
     }
