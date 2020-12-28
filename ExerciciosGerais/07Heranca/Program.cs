b02992e7fe17f6133e92c63c52b19d9091cc65cd
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 17:36:02 -0300

    Test implementation application

--- ExerciciosGerais/07Heranca/Program.cs
+++ ExerciciosGerais/07Heranca/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using _07Heranca.Entities;
 
 namespace _07Heranca
 {
@@ -6,7 +7,14 @@ namespace _07Heranca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc1 = new Account(1002, "Marcelo", 500.0);
+            Account acc2 = new SavingAccount(1003, "Marcelo", 500.0, 0.01);
+
+            acc1.WithDraw(10.0);
+            acc2.WithDraw(10.0);
+
+            Console.WriteLine($"Saque da conta Account = {acc1.Balance}");
+            Console.WriteLine($"Saque da conta SavingAccount = {acc2.Balance}");
         }
     }
 }
