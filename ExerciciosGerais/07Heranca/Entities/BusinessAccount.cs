1565b037e66cbebc9796fae20b5cae4b9d997daa
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 17:23:46 -0300

    Creating BusinessAccount Class subclass Account

---
+++ ExerciciosGerais/07Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,24 @@
+namespace _07Heranca.Entities
+{
+    public class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+            {
+                LoanLimit = loanLimit;
+            }
+        
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
