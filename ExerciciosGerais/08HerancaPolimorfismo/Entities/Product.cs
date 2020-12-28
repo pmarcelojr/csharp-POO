d2a7e56ed23b6d8b5717f46f714a05ee4e15a90a
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 20:03:50 -0300

    Including private price

--- ExerciciosGerais/08HerancaPolimorfismo/Entities/Product.cs
+++ ExerciciosGerais/08HerancaPolimorfismo/Entities/Product.cs
@@ -5,7 +5,7 @@ namespace _08HerancaPolimorfismo.Entities
     public class Product
     {
         public string Name { get; set; }
-        public double Price { get; set; }
+        public double Price { get; private set; }
 
         public Product() { }
         public Product(string name, double price)
