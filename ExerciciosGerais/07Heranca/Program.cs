df1ce6c6910f2307ec2f87ffd897844a8a315211
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 17:13:56 -0300

    Creating Heranca Project ExerciciosGerais

---
+++ ExerciciosGerais/07Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
