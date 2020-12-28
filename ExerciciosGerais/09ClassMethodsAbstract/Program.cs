0d206997a3a101014a23aeb9fc3d102ffff6d738
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 20:49:25 -0300

    Created 09ClassMethodsAbstract

---
+++ ExerciciosGerais/09ClassMethodsAbstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09ClassMethodsAbstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
