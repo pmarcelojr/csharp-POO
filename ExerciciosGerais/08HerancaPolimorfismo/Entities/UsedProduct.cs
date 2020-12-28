f36886b7f799c7daf553bad5d0fab122ec24d737
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 19:46:46 -0300

    Creating UsedProduct subclass Product

---
+++ ExerciciosGerais/08HerancaPolimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace _08HerancaPolimorfismo.Entities
+{
+    public class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct() { }
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+            {
+                ManufactureDate = manufactureDate;
+            }
+
+        public override string PriceTag()
+        {
+            return $"{Name} (used) $ {Price} (Manufacture date: {ManufactureDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
