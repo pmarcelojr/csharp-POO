8781ea9997adb2339be8c13cd992857221bf8fbd
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 19:47:11 -0300

    Creating logic program with usedProduct and Product

--- ExerciciosGerais/08HerancaPolimorfismo/Program.cs
+++ ExerciciosGerais/08HerancaPolimorfismo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using _08HerancaPolimorfismo.Entities;
 
 namespace _08HerancaPolimorfismo
 {
@@ -6,7 +9,39 @@ namespace _08HerancaPolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Product #{i + 1} data: ");
+                Console.Write("Common, Used or Imported (C/U/I)? ");
+                char typeProduct = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(typeProduct == 'u' || typeProduct == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("PRICE TAGS:");
+            foreach(Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
         }
     }
 }
