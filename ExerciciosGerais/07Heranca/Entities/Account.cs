507432099415dfa48bbcadf237ea808213adcc14
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 17:18:53 -0300

    Creating Account Class Base

---
+++ ExerciciosGerais/07Heranca/Entities/Account.cs
@@ -0,0 +1,29 @@
+namespace _07Heranca.Entities
+{
+    public class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
