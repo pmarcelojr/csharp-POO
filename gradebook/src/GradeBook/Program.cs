fe192735901faabe25171fc7453eff010a8fed99
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 22:58:02 -0300

    modify type book to IBook

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,14 +7,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new InMemoryBook("Scott's Grade Book");
+            IBook book = new DiskBook("Scott's Grade Book");
             book.GradeAdded += OnGradeAdded;
 
             EnterGrades(book);
 
             var stats = book.GetStatistics();
 
-            Console.WriteLine(InMemoryBook.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The High grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
