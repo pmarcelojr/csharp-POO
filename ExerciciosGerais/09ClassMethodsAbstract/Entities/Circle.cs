b3192861907b27c4272630f6b17ac5776962d558
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 21:35:25 -0300

    Created Circle subclass shape

---
+++ ExerciciosGerais/09ClassMethodsAbstract/Entities/Circle.cs
@@ -0,0 +1,20 @@
+using _09ClassMethodsAbstract.Entities.Enums;
+using System;
+
+namespace _09ClassMethodsAbstract.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(Color color, double radius) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
