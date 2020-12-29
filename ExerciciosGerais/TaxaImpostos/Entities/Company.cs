a03782b42bcccf5619d1ec83feff2bacf30d6cbc
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-29 12:23:37 -0300

    Created Company Subclass

---
+++ ExerciciosGerais/TaxaImpostos/Entities/Company.cs
@@ -0,0 +1,26 @@
+
+namespace TaxaImpostos.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company() {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) 
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax() {
+            if (NumberOfEmployees > 10) {
+                return AnualIncome * 0.14;
+            }
+            else {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
