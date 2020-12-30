0031a31ee5a6f0783cf807300f05446071f6f3ab
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-30 12:48:19 -0300

    Solution old in program Exer 10ExcecoesPersonalizadas

--- ExerciciosGerais/10ExcecoesPersonalizadas/Program.cs
+++ ExerciciosGerais/10ExcecoesPersonalizadas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using _10ExcecoesPersonalizadas.Entities;
 
 namespace _10ExcecoesPersonalizadas
 {
@@ -6,7 +7,44 @@ namespace _10ExcecoesPersonalizadas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-In date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if(checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-Out date must be a after Check-In");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine($"Reservation:\n{reservation}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if(checkin < now || checkout < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if(checkout <= checkin)
+                {
+                    Console.WriteLine("Error in reservation: Check-Out date must be after Check-In date");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkin, checkout);
+                    Console.WriteLine($"Reservation Update:\n{reservation}");
+                }
+            }
         }
     }
 }
