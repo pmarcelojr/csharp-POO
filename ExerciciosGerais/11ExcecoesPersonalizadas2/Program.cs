1f503cac883b62405e3cafd5bc8ce7dcad7fcb0e
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-30 16:24:50 -0300

    updating logic try/catch

--- ExerciciosGerais/11ExcecoesPersonalizadas2/Program.cs
+++ ExerciciosGerais/11ExcecoesPersonalizadas2/Program.cs
@@ -9,25 +9,25 @@ namespace _11ExcecoesPersonalizadas2
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: ");
+            double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withDrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             try
             {
-                Console.Clear();
-                Console.WriteLine("Enter account data:");
-                Console.Write("Number: ");
-                int number = int.Parse(Console.ReadLine());
-                Console.Write("Holder: ");
-                string holder = Console.ReadLine();
-                Console.Write("Initial Balance: ");
-                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Withdraw Limit: ");
-                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Account account = new Account(number, holder, balance, withDrawLimit);
-
-                Console.WriteLine();
-                Console.Write("Enter amount for withdraw: ");
-                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
                 account.WithDraw(amount);
                 Console.WriteLine(account);
             }
