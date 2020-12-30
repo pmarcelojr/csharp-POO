cd8f3e02fe850b105b679967bb02dfcf17ecab95
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-30 12:28:14 -0300

    Creating Reservation Class in Exer 10ExcecoesPersonalizadas

---
+++ ExerciciosGerais/10ExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace _10ExcecoesPersonalizadas.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut; 
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return $"Room {RoomNumber}, Check-In: {CheckIn.ToString("dd/MM/yyyy")}, Check-Out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
