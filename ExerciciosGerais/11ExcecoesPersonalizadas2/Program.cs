13e43b3a4fddc5c818730d7eb50f15b12dbea4d0
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-30 16:19:10 -0300

    Created Logic program

--- ExerciciosGerais/11ExcecoesPersonalizadas2/Program.cs
+++ ExerciciosGerais/11ExcecoesPersonalizadas2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using _11ExcecoesPersonalizadas2.Entities;
+using _11ExcecoesPersonalizadas2.Entities.Exceptions;
 
 namespace _11ExcecoesPersonalizadas2
 {
@@ -6,7 +9,36 @@ namespace _11ExcecoesPersonalizadas2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withDrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.WithDraw(amount);
+                Console.WriteLine(account);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format Error: {e.Message}");
+            }
         }
     }
 }
