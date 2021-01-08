33d175d31fbb63612badd8fe4e8921edb6686fb9
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-08 16:29:17 -0300

    Introduction to Directory and DirectoryInfo

--- ExerciciosGerais/16DirectoryAndDirectoryInfo/Program.cs
+++ ExerciciosGerais/16DirectoryAndDirectoryInfo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace _16DirectoryAndDirectoryInfo
 {
@@ -6,7 +8,37 @@ namespace _16DirectoryAndDirectoryInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = @"C:\temp\myfolder";
+
+            try
+            {
+                // Lista as pastas a partir de uma pasta informada
+                IEnumerable<string> folders = Directory.EnumerateDirectories(sourcePath, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders:");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                
+                Console.WriteLine("---------------------------------------");
+                // Lista os arquivos a partir de uma pasta informada
+                IEnumerable<string> files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files:");
+                foreach(string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine("---------------------------------------");
+                // Criar uma pasta
+                Directory.CreateDirectory($@"{sourcePath}\newFolder");
+                Console.WriteLine("Nova pasta criada!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);                
+            }
         }
     }
 }
