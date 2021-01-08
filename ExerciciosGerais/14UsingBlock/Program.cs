4fda9b438f1fc1b3a326c2c3cb5a247cd7019b69
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-08 13:14:43 -0300

    Introduction to UsingBlock with System.IO

--- ExerciciosGerais/14UsingBlock/Program.cs
+++ ExerciciosGerais/14UsingBlock/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _14UsingBlock
 {
@@ -6,7 +7,24 @@ namespace _14UsingBlock
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = @"C:\temp\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
