1190f337099a495636e3567d3a76a004cdeb0e76
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-08 19:22:54 -0300

    Introduction to StreamWrite second

--- ExerciciosGerais/15a_StreamWriter/Program.cs
+++ ExerciciosGerais/15a_StreamWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _15a_StreamWriter
 {
@@ -6,7 +7,26 @@ namespace _15a_StreamWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // // 1: Escreve uma linha para novo arquivo
+            // using (StreamWriter sw = new StreamWriter(@"C:\temp\macoratti.txt", true))
+            // {
+            //     sw.WriteLine("Macorrati .net");
+            // }
+            // // 2: Anexa uma linha ao arquivo
+            // using (StreamWriter sw = new StreamWriter(@"C:\temp\macoratti.txt", true))
+            // {
+            //     sw.WriteLine("Quase tudo para .net");
+            // }
+            /////////////////////////////////////////////////////////////////////////////
+            using(var sw = new StreamWriter(@"C:\temp\macoratti.txt"))
+            {
+                // Percorre o laço
+                for (int i = 0; i < 10; i++)
+                {
+                    // Escreve uma string formatada
+                    sw.WriteLine("{0:0.0} ", i);
+                }
+            }
         }
     }
 }
