03670353b002216d520631552095a4f1518a376b
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-08 12:33:09 -0300

    Created exerc 13FileStreamAndStreamReader

---
+++ ExerciciosGerais/13FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
