774bc75b567234aaf2b816c492a86018799b8f63
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 15:26:20 -0300

    Exerc ArquivosCsharpIO/WriteALLText

---
+++ ExerciciosGerais/ArquivosCsharpIO/WriteAllText/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace WriteAllText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Isto é uma string";
+            File.WriteAllText("arquivo1.txt", texto);
+
+            Console.ReadKey();
+        }
+    }
+}
