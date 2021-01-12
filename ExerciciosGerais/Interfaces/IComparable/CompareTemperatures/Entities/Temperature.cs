9e3d5ea0521a798cd1a0194da5525b6f047c49d3
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 14:09:44 -0300

    Implementation in Exerc CompareTemperatures in interfaces IComparable

---
+++ ExerciciosGerais/Interfaces/IComparable/CompareTemperatures/Entities/Temperature.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace CompareTemperatures.Entities
+{
+    class Temperature : IComparable
+    {
+        // The temperature value
+        protected double temperatureF;
+
+        public int CompareTo(object obj)
+        {
+            if(obj == null)  return 1;
+
+            Temperature otherTemperature = obj as Temperature;
+            if(otherTemperature != null)
+            {
+                return this.temperatureF.CompareTo(otherTemperature.temperatureF);
+            }
+            else
+            {
+                throw new ArgumentException("Object is not a temperature.");
+            }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return this.temperatureF;
+            }
+            set
+            {
+                this.temperatureF = value;
+            }
+        }
+
+        public double Celsius
+        {
+            get
+            {
+                return (this.temperatureF - 32) * (5.0/9);
+            }
+            set
+            {
+                this.temperatureF = (value * 9.0/5) + 32;
+            }
+        }
+    }
+}
--- ExerciciosGerais/Interfaces/IComparable/CompareTemperatures/Program.cs
+++ ExerciciosGerais/Interfaces/IComparable/CompareTemperatures/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CompareTemperatures.Entities;
 
 namespace CompareTemperatures
 {
@@ -6,7 +9,26 @@ namespace CompareTemperatures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList temperatures = new ArrayList();
+            // Initialize random number generator
+            Random rnd = new Random();
+
+            // Generate 10 temperatures betweem 0 and 100 randonly
+            for (int ctr = 1; ctr <= 10; ctr++)
+            {
+                int degrees = rnd.Next(0, 100);
+                Temperature temp = new Temperature();
+                temp.Fahrenheit = degrees;
+                temperatures.Add(temp);
+            }
+
+            // Sort ArrayList
+            temperatures.Sort();
+
+            foreach (Temperature temp in temperatures)
+            {
+                Console.WriteLine(temp.Fahrenheit);
+            }
         }
     }
 }
