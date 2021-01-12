97c5e5988de048935ac5b0c7ddf25e719f3aa8f0
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 13:56:49 -0300

    Created Exerc CompateTemperatures with IComparable

---
+++ ExerciciosGerais/Interfaces/IComparable/CompareTemperatures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompareTemperatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
