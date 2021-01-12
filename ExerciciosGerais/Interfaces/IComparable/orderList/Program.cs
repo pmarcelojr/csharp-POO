95effe72694dd2d00935964f49c26ec3f894a270
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 12:31:05 -0300

    Created Exerc order List with IComparable

---
+++ ExerciciosGerais/Interfaces/IComparable/orderList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace orderList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ExerciciosGerais/Interfaces/IComparable/orderList/README.md
@@ -0,0 +1,14 @@
+## Problema motivador
+
+Faça um programa para ler um arquivo contendo nomes de pessoas (um nome por linha), armazenando-os em uma lista. Depois, ordenar os dados dessa lista e mostra-los ordenadamente na tela.
+
+Nota: O caminho do arquivo pode ser informado <i>"hardcode"</i>
+
+Maria Brown
+Alex green
+Bob Grey
+Anna White
+Eduardo Rose
+Willian Red
+Marta Blue
+Alex Brown
