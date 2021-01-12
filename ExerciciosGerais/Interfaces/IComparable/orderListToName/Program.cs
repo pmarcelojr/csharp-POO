20869ce88b2355133a6978880a73fa2e2ad92861
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 12:53:01 -0300

    Created Exerc oderListtoName with IComparable interface

---
+++ ExerciciosGerais/Interfaces/IComparable/orderListToName/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using orderListToName.Entities;
+
+namespace orderListToName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ExerciciosGerais/Interfaces/IComparable/orderListToName/README.md
@@ -0,0 +1,14 @@
+### Problema motivador
+
+faça um programa para ler um arquivo contendo funcionários (nome e salário). Armazenando-os em uma lista. Depois, ordernar a lista por nome e mostrar o resultado na tela.
+
+Nota: O caminho do arquivo pode ser informado <i>"hardcord"</i>
+
+Maria Brown,4300.00
+Alex green,3100.00
+Bob Grey,3100.00
+Anna White,3500.00
+Eduardo Rose,4390.00
+Willian Red,2900.00
+Marta Blue,6100.00
+Alex Brown,5000.00
