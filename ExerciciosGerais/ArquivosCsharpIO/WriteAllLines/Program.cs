89b21b49626f22d77278342b8dc9b19421edba64
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 15:30:59 -0300

    Exerc ArquivosCsharpIO/WriteAllLines

---
+++ ExerciciosGerais/ArquivosCsharpIO/WriteAllLines/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace WriteAllLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = {"Primeira sentença", "Segunda sentença", "Terceira sentença" };
+
+            File.WriteAllLines("arquivo1.txt", lines);
+        }
+    }
+}
