2f730fbb4c7c0298c3f8d829b1a62b91ca14a7bf
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 18:06:37 -0300

    Created exerc GetHashCodeAndEquals

---
+++ ExerciciosGerais/generics/GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
