b9d77285f1337ada4749482546a6f0e17827e0f0
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 16:03:37 -0300

    Implementatio logic program in exerc GenericsTwo

--- ExerciciosGerais/generics/genericsTwo/Program.cs
+++ ExerciciosGerais/generics/genericsTwo/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using genericsTwo.Services;
+using genericsTwo.Entities;
+using System.Globalization;
 
 namespace genericsTwo
 {
@@ -6,7 +10,24 @@ namespace genericsTwo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive: ");
+            Console.WriteLine(max);
         }
     }
 }
