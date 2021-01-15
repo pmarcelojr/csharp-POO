4a57833c06d10bdac30bb4cb383c06fc32301966
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 18:48:32 -0300

    Created exerc SortedSetExercicio

---
+++ ExerciciosGerais/generics/SortedSetExercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortedSetExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
