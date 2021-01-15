28cb25db7ccf066d4b19bf491a644105de903809
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 19:58:07 -0300

    Implementation simple example in HashIgualdade

--- ExerciciosGerais/generics/HashIgualdade/Program.cs
+++ ExerciciosGerais/generics/HashIgualdade/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using HashIgualdade.Entities;
 
 namespace HashIgualdade
 {
@@ -6,7 +8,19 @@ namespace HashIgualdade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1200.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
         }
     }
 }
