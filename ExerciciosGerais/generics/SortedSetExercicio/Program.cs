2b2b230f79b16e5ec042011c3306b0d00e724aaf
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 19:03:58 -0300

    Implementation simple example in SortedSetExercicio

--- ExerciciosGerais/generics/SortedSetExercicio/Program.cs
+++ ExerciciosGerais/generics/SortedSetExercicio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortedSetExercicio
 {
@@ -6,7 +7,35 @@ namespace SortedSetExercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SortedSet<int> a = new SortedSet<int>() { 10, 2, 4, 5, 6, 8, 15 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 20, 9, 10 };
+
+            // Union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            Console.Write("Union: ");
+            PrintCollection(c);
+
+            // Intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            Console.Write("Intersection: ");
+            PrintCollection(d);
+
+            // Difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            Console.Write("Difference: ");
+            PrintCollection(e);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
         }
     }
 }
