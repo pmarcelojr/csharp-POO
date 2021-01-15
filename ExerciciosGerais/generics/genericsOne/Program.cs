6a14cda7f4cb0e08d992936f9bb4743f822d6d84
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-15 15:12:16 -0300

    Class parameter type in exerc GenericsOne

--- ExerciciosGerais/generics/genericsOne/Program.cs
+++ ExerciciosGerais/generics/genericsOne/Program.cs
@@ -7,12 +7,12 @@ namespace genericsOne
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 int x = int.Parse(Console.ReadLine());
                 printService.AddValue(x);
