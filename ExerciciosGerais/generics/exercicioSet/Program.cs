8943b716672bba552a2319421c635b16eead3bf4
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 14:14:27 -0300

    Created exerc exercicioSet

---
+++ ExerciciosGerais/generics/exercicioSet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicioSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
