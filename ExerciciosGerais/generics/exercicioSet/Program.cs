f1d951d0b29ae5f776a5156e8c0a44d21fa7167f
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 14:50:58 -0300

    Initial Implementation exercicioSet

--- ExerciciosGerais/generics/exercicioSet/Program.cs
+++ ExerciciosGerais/generics/exercicioSet/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using exercicioSet.Entities;
 
 namespace exercicioSet
 {
@@ -6,7 +9,29 @@ namespace exercicioSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vect = sr.ReadLine().Split(' ');
+                        string name = vect[0];
+                        DateTime instant = DateTime.Parse(vect[1]); 
+                        set.Add(new LogRecord { UserName = name, Instant = instant } );
+                    }
+                    Console.WriteLine($"Total users: {set.Count}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
