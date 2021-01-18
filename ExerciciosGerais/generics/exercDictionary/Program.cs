f6bdf7f0d2c9505bba5227dfee48a20ac43420ec
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 16:10:40 -0300

    Implementation simple exercicio Dictionary

--- ExerciciosGerais/generics/exercDictionary/Program.cs
+++ ExerciciosGerais/generics/exercDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exercDictionary
 {
@@ -6,7 +7,32 @@ namespace exercDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "9932323211";
+            cookies["phone"] = "5423012341";
+
+            Console.WriteLine(cookies["phone"]);
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+            
+            if(cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("there is no 'email' key ");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("All cookies: ");
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
         }
     }
 }
