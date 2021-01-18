830e2fe8e3bb245e7ea147c2c2e96c473b4e5ff3
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 17:07:53 -0300

    Created exercicio Extension method elapsed time

---
+++ ExerciciosGerais/exercMethodElapsedTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercMethodElapsedTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
