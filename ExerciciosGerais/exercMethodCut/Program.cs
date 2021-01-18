2d79b6242d3bb2550800110065c4ced3fabe109f
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 17:35:00 -0300

    Created exercicio Extension method cut

---
+++ ExerciciosGerais/exercMethodCut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercMethodCut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
