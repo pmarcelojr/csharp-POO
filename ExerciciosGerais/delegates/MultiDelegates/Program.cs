274519bfd1bfba2d92f2e96fef10fd628d298b74
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 16:34:43 -0300

    Created exerc MultiDelegates

---
+++ ExerciciosGerais/delegates/MultiDelegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
