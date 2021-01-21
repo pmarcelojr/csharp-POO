278b8d1d230fd1fa4633524e6a5cf0cc4ffe3ead
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 16:27:03 -0300

    Implementation introduction delegate with exerc IntroDelegates

--- ExerciciosGerais/delegates/IntroDelegates/Program.cs
+++ ExerciciosGerais/delegates/IntroDelegates/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using IntroDelegates.Services;
 
 namespace IntroDelegates
 {
+    delegate double BinaryNumericOperation(double n1, double n2);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 10;
+            double b = 12;
+            
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op(a, b);
+            Console.WriteLine(result);
         }
     }
 }
