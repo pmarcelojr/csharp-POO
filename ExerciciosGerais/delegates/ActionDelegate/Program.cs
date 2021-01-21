bf3661226c72290563dad42eab79d7c56142eb67
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 17:30:16 -0300

    Implementation exerc ActionDelegate

--- ExerciciosGerais/delegates/ActionDelegate/Program.cs
+++ ExerciciosGerais/delegates/ActionDelegate/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ActionDelegate.Entities;
 
 namespace ActionDelegate
 {
@@ -6,7 +8,29 @@ namespace ActionDelegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.00));
+
+            //Action<Product> act = p => { p.Price += p.Price * 0.1 };
+            Action<Product> act = UpdatePrice;
+
+            list.ForEach(act);
+            //list.ForEach(UpdatePrice);
+            //list.ForEach(p => p.Price += p.Price * 0.1 );
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
