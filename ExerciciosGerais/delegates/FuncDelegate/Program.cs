e44bc78b2caa3feb2cffddb4a6a68f714abe619d
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 18:00:44 -0300

    Implementation exerc FuncDelegate

--- ExerciciosGerais/delegates/FuncDelegate/Program.cs
+++ ExerciciosGerais/delegates/FuncDelegate/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
+using FuncDelegate.Entities;
+using System;
 
 namespace FuncDelegate
 {
@@ -6,7 +9,29 @@ namespace FuncDelegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.00));
+
+            Func<Product, string> func = p => p.Name.ToUpper();
+            //Func<Product, string> func = NameUpper;
+            List<string> result = list.Select(func).ToList();
+            
+            //List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+            //List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (string p in result)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
     }
 }
