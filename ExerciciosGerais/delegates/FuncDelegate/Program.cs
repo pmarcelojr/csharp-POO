e3dbefedd06da090bd526ed82b2385bfdf34fa44
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 17:40:30 -0300

    Created exerc FuncDelegate

---
+++ ExerciciosGerais/delegates/FuncDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
