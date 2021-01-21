a3e9abbe78d48c54f2ad58d2c8d63bd8904fd34d
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-21 16:53:03 -0300

    Created exerc PredicateDelegate

---
+++ ExerciciosGerais/delegates/PredicateDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PredicateDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
