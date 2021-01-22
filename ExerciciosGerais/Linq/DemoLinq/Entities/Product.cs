f11282ebdbc648c985e49a4e1410c48b36b86b74
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-22 13:45:06 -0300

    Implementation exerc DemoLinq initial

---
+++ ExerciciosGerais/Linq/DemoLinq/Entities/Product.cs
@@ -0,0 +1,17 @@
+using System.Globalization;
+
+namespace DemoLinq.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Price.ToString("f2", CultureInfo.InvariantCulture)}, {Category.Name}, {Category.Tier}";
+        }
+    }
+}
--- ExerciciosGerais/Linq/DemoLinq/Program.cs
+++ ExerciciosGerais/Linq/DemoLinq/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using DemoLinq.Entities;
 
 namespace DemoLinq
 {
@@ -6,7 +9,80 @@ namespace DemoLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+
+            List<Product> products = new List<Product>() { 
+                new Product() { Id = 1, Name = "Computer", Price = 1100.00, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.00, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.00, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.00, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.00, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.00, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.00, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.00, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.00, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.00, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.00, Category = c1 },
+            };
+
+            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            Print("TIER 1 AND PRICE < 900", r1);
+
+            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+
+            var r3 = products.Where(p => p.Name[0].Equals('C')).Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("TIER 1 ORDER BY PRICE AND THEN BY NAME", r4);
+
+            var r5 = r4.Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE AND THEN BY NAME SKIP 2 TAKE 4", r5);
+
+            var r6 = products.FirstOrDefault();
+            Console.WriteLine($"First or default test 1: {r6}");
+            var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            Console.WriteLine($"First or default test 2: {r7}");
+            Console.WriteLine();
+
+            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
+            Console.WriteLine($"Single or default test 1: {r8}");
+            var r9 = products.Where(p => p.Id == 20).SingleOrDefault();
+            Console.WriteLine($"Single or default test 2: {r9}");
+            Console.WriteLine();
+            
+            var filteredOrders = products.Where(p => Filter(p.Name, "So Ba"));
+            Print("PESQUISA: ", filteredOrders);
+        }
+
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach (T obj in collection)
+            {
+               Console.WriteLine(obj); 
+            }
+            Console.WriteLine();
+        }
+
+        // Pesquisa que permite a busca por partes da descrição, ex: Products chamado "Sound Bar", se pesquisar "so ba" ele encontre
+        static bool Filter(string word, string keyword)
+        {
+            var keywords = keyword.Split(" ");
+            if (keywords.Length > 0 && !string.IsNullOrEmpty(keyword))
+            {
+                foreach ( var item in keywords)
+                {
+                    if ( word.Contains(item, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
