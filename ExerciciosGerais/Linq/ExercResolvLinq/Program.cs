12b113c390bc3ed48a28ee4f550c4240b4390693
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-22 16:49:07 -0300

    Implementation exerc ExercResolvLinq

--- ExerciciosGerais/Linq/ExercResolvLinq/Program.cs
+++ ExerciciosGerais/Linq/ExercResolvLinq/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Globalization;
+using ExercResolvLinq.Entities;
+using System.IO;
+using System.Collections.Generic;
 
 namespace ExercResolvLinq
 {
@@ -6,7 +11,30 @@ namespace ExercResolvLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> listProduct = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(",");
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    listProduct.Add(new Product(name, price));
+                }
+            }
+
+            var avg = listProduct.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Average price = {avg.ToString("f2", CultureInfo.InvariantCulture)}");
+
+            var names = listProduct.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
