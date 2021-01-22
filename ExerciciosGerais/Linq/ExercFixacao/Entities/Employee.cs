34631409b9babd7c17f68998f5412e8e9824f46c
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-22 18:06:19 -0300

    Created entities in ExercFixacao in Linq

---
+++ ExerciciosGerais/Linq/ExercFixacao/Entities/Employee.cs
@@ -0,0 +1,23 @@
+using System.Globalization;
+
+namespace ExercFixacao.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}, {Salary.ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
