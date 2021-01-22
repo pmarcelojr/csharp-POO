b54289092db88787b9bb9abbd76bfa5e2c4b03a3
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-22 16:46:55 -0300

    Created Entities exerc ExercResolvLinq

---
+++ ExerciciosGerais/Linq/ExercResolvLinq/Entities/Product.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+
+namespace ExercResolvLinq.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
