5037e1a6a929fe39ba0ae4332707e5eef6eaa37e
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-22 18:06:38 -0300

    Implementation exerc ExercFixacao in Linq

--- ExerciciosGerais/Linq/ExercFixacao/Program.cs
+++ ExerciciosGerais/Linq/ExercFixacao/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using ExercFixacao.Entities;
 
 namespace ExercFixacao
 {
@@ -6,7 +11,45 @@ namespace ExercFixacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(",");
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
+                }
+                Console.WriteLine();
+                Console.Write("Enter salary: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine($"Email of people whose salary is more than {value.ToString("f2", CultureInfo.InvariantCulture)}");
+
+                var emails = list.Where(p => p.Salary > value).OrderBy(p => p.Name).Select(p => p.Email);
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                Console.WriteLine();
+                var sumSalary = list.Where(p => p.Name[0].Equals('M')).Sum(p => p.Salary);
+                Console.Write($"Sum of salary of people whose name starts with 'M': {sumSalary.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
