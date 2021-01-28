b7e9a04fe5977d1714f833f37a0a8b1861c8b3b8
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-28 15:09:26 -0300

    Created Constructors entities

--- MVC_Projects/SalesWebMVC/Models/Seller.cs
+++ MVC_Projects/SalesWebMVC/Models/Seller.cs
@@ -12,5 +12,19 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
     }
 }
