b4658bbfe8c9e61f7b5e5b4449d3dc41436f0a67
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-28 18:30:53 -0300

    Created Delete Seller controller and service routes

--- MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
+++ MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
@@ -29,7 +29,7 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments }; 
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
@@ -38,6 +38,30 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
