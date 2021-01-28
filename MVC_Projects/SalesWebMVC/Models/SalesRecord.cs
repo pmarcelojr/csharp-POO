ffa115f731acc1837571486fa833b262dfda980b
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-28 15:04:41 -0300

    Implementation association entities and use ICollection

---
+++ MVC_Projects/SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,14 @@
+using System;
+using SalesWebMVC.Models.Enums;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
