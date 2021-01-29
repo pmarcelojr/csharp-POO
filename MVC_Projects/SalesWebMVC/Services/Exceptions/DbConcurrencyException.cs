03afa638ebf1d4cc322edad18ae7379ce633ed58
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-29 17:14:55 -0300

    Created Exceptions custom in Services

---
+++ MVC_Projects/SalesWebMVC/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
