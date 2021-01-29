34b84c7de87db245a15569b8818ff2314d0317a7
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-29 18:21:43 -0300

    Validation in controller sellers

--- MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
+++ MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
@@ -39,6 +39,12 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -105,6 +111,12 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
@@ -116,7 +128,7 @@ namespace SalesWebMVC.Controllers
             }
             catch (NotFoundException e)
             {
-                return RedirectToAction(nameof(Error), new { message = e.Message });;
+                return RedirectToAction(nameof(Error), new { message = e.Message }); ;
             }
             catch (DbConcurrencyException e)
             {
