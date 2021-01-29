fbdb4cabffee5065243fab61acbaf91944e297fd
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-29 17:33:06 -0300

    Created action edit post Seller

--- MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
+++ MVC_Projects/SalesWebMVC/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
+using SalesWebMVC.Services.Exceptions;
 //using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
@@ -97,6 +98,29 @@ namespace SalesWebMVC.Controllers
             List<Department> departments = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
