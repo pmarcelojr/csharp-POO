c865b00cc55e8eb4cc9305463f36aedb81b0416d
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-11 12:32:24 -0300

    Created folder EFCode and project Introduction

---
+++ EFCode/Introduction/Migrations/20210211152155_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Introduction.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livro",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Autor = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    AnoPublicacao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livro", x => x.LivroId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livro");
+        }
+    }
+}
---
+++ EFCode/Introduction/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new LivroContext())
+            {
+                db.Livros.Add(new Livro { Titulo = "Domain-Driven Desing", Autor = "Eric Evans", AnoPublicacao = 2003 });
+                db.Livros.Add(new Livro { Titulo = "Agile principles", Autor = "Robert C. Martin", AnoPublicacao = 2006 });
+                db.SaveChanges();
+
+                Console.WriteLine("------------------- RESULTADOS BANCO DE DADOS --------------------");
+                db.Livros.ForEachAsync(x =>
+                    Console.WriteLine($"Titulo: {x.Titulo} | Autor: {x.Autor} | Publicação: {x.AnoPublicacao}"));
+            }
+        }
+    }
+
+    public class LivroContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql(@"server=localhost; port=3306; database=DeveloperExpert; user=root; password=1234; Persist Security Info=False; Connect Timeout=300");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Livro>()
+                .ToTable("Livro");
+
+            modelBuilder.Entity<Livro>()
+                .HasKey(p => p.LivroId);
+            
+            modelBuilder.Entity<Livro>()
+                .Property(p => p.Titulo)
+                .HasColumnType("varchar(50)");
+        }
+    }
+
+    public class Livro
+    {
+        public int LivroId { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+    }
+}
