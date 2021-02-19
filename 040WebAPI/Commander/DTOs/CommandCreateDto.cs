fda43089baf43525fb198ac10221b4e6c2b4a638
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-19 16:36:09 -0300

    Creating CommandCreateDto

---
+++ 040WebAPI/Commander/DTOs/CommandCreateDto.cs
@@ -0,0 +1,9 @@
+namespace Commander.DTOs
+{
+    public class CommandCreateDto
+    {
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
